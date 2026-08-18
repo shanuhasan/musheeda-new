8aca3ca1793ab080ef5553750928bbda31e8bc8e
Shahnawaj <shahnawaj@inspheresolutions.com>
2026-08-18 23:41:03 +0530

    updates

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -176,7 +176,7 @@
 @endsection
 
 @push('scripts')
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.8.3/tinymce.min.js" referrerpolicy="origin"></script>
 <script>
     tinymce.init({
         selector: '.tinymce',
